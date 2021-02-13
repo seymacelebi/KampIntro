1e379b2e7a3331809fedf374d7bfd0943b0fdc67
seymacelebi <seymaesmaa198@gmail.com>
2021-02-14 02:36:22 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 3200;
+            double faizOranı = 1.45;
+            bool sistemeGirisYapmısMı = false;
+
+            if (sistemeGirisYapmısMı==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
